ec3939bb0804aec60e5526fac392a4f5fa202a06
Ariful Islam Iurjoy <arifulislamdurjoy3111@gmail.com>
2023-03-07 23:38:57 +0600

    just create the project and push it

---
+++ lib/Bottom_Nav_Bar/Bottom_Nav_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bottom_Nav_controller extends StatelessWidget {
+  const Bottom_Nav_controller({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Bottom_Nav_Bar/Screens/Meet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Meet extends StatelessWidget {
+  const Meet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
